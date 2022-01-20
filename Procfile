ba3d53032ddfd7efcdddecbca9163b87e96f4348
TheVatsu <66404934+TheVatsu@users.noreply.github.com>
2022-01-20 14:15:25 +0100

    clean git

--- Procfile
+++
@@ -1,2 +0,0 @@
-web: python soap-vehicule/server.py
-worker: python soap-vehicule/server.py 
--- soap-vehicule/client.js
+++
@@ -1,8 +0,0 @@
-var soap = require('soap');
-var url = 'http://127.0.0.1:8000/?wsdl';
-var args = {};
-soap.createClient(url, function(err, client) {
-    client.get_vehicules(args, function(err, result) {
-        console.log(result.get_vehiculesResult.data);
-    });
-})
