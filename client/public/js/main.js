86d1a5b875144f8110487b139a9874503455d95a
TheVatsu <66404934+TheVatsu@users.noreply.github.com>
2022-02-03 11:56:42 +0100

    aaa

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -2,7 +2,7 @@
 var vehicule = []
 
 $( document ).ready(function() {
-    const socket = io("https://carrera-info802.herokuapp.com/");// "http://localhost:3000"
+    const socket = io("http://localhost:3000");//"https://carrera-info802.herokuapp.com/"
 
     socket.on("vehicule", (data) => {
       $('#combo_box').text("")
@@ -20,6 +20,23 @@ $( document ).ready(function() {
             $('.autonomie').text(el.autonomy + "km")
             $('.charging_time').text(el.charge_time)
       }
+  });
+
+  socket.on("res_temps", (data) => {
+     $('.temps_total').text(data);
+  });
+
+  
+  $("#bouton_temps").click(function(){
+    var path = "national";
+    var km = $('#nb_km').val();
+    var el = vehicule.filter(el => el.name === $('#combo_box').val())[0];
+    if(el != undefined && km != ''){
+      if(($('#combo_box_road').val()) === "Autoroute"){
+        path = "highway";
+      }
+      socket.emit(path,km,el.autonomy,parseInt(el.charge_time));
+    }
   });
     
 });
--- client/server.js
+++ client/server.js
@@ -1,6 +1,7 @@
 var soap = require('soap');
 const express = require('express');
 const socketIO = require('socket.io');
+const request = require('request');
 
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
@@ -12,6 +13,7 @@ const server = express().use(express.static("public"))
 const io = socketIO(server);
 
 var url_soap = 'https://vehicule-soap.herokuapp.com/?wsdl';
+var url_rest = 'https://rest-vehicule.herokuapp.com/';
 var args = {};
 var vehicule = []
 soap.createClient(url_soap, function(err, client) {
@@ -23,4 +25,20 @@ soap.createClient(url_soap, function(err, client) {
 
 io.on('connection',(socket)=>{
   io.to(socket.id).emit("vehicule",vehicule);
+
+  socket.on('highway',(km,autonomy,loading_time)=>{
+    console.log(km + " " + autonomy + " " + loading_time)
+    request(url_rest + 'highway?km='+km+'&autonomy='+autonomy+'&loading_time='+loading_time, function (error, response, body) {
+       io.to(socket.id).emit("res_temps",body);
+    });
+  });
+
+  socket.on('national',(km,autonomy,loading_time)=>{
+    request(url_rest + 'national?km='+km+'&autonomy='+autonomy+'&loading_time='+loading_time, function (error, response, body) {
+       io.to(socket.id).emit("res_temps",body);
+    });
+  });
+
 });
+
+
